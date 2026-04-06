27efbfa2895c02335dc76ca6c004a1234824d27d
Cory Martin <Cory.R.Martin@noaa.gov>
2026-04-06 14:19:48 +0000

    start of norms

--- src/ijedi/FieldMetadata/FieldsMetadata.h
+++ src/ijedi/FieldMetadata/FieldsMetadata.h
@@ -17,7 +17,7 @@ namespace ijedi
 
   class FieldMetadata : public util::Printable
   {
-  public:
+   public:
     explicit FieldMetadata(const std::string longName, int nlev)
     {
       longName_ = longName;
@@ -49,16 +49,13 @@ namespace ijedi
       if (numLevls == "full")
       {
         numLevls_ = nlev_;
-      }
-      else if (numLevls == "half")
+      } else if (numLevls == "half")
       {
         numLevls_ = nlev_ + 1;
-      }
-      else if (numLevls == "halfplusone")
+      } else if (numLevls == "halfplusone")
       {
         numLevls_ = nlev_ + 2;
-      }
-      else
+      } else
       {
         try
         {
@@ -79,12 +76,10 @@ namespace ijedi
       if (tracer == "true")
       {
         isTracer_ = true;
-      }
-      else if (tracer == "false")
+      } else if (tracer == "false")
       {
         isTracer_ = false;
-      }
-      else
+      } else
       {
         ABORT("FieldMetadata::setIsTracer tracer must be true or false");
       }
@@ -108,7 +103,7 @@ namespace ijedi
       this->validateVariable(ValidGridMask_, gridMask_);
     }
 
-  private:
+   private:
     // Picked up from default file
     std::string longName_;
     std::string dataKind_;
@@ -153,7 +148,7 @@ namespace ijedi
 
   class FieldsMetadata : public util::Printable
   {
-  public:
+   public:
     explicit FieldsMetadata(const int);
 
     // Get FieldMetadata from any of the potential field names
@@ -165,7 +160,7 @@ namespace ijedi
     // Function to return all the long names
     const std::vector<std::string> &getLongNames() const { return longNames_; }
 
-  private:
+   private:
     std::map<std::string, FieldMetadata> fieldsMetadata_;
     std::vector<std::string> longNames_;
 
@@ -184,4 +179,4 @@ namespace ijedi
 
   // -----------------------------------------------------------------------------------------------
 
-} // namespace ijedi
+}  // namespace ijedi
